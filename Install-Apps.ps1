fa2a186690ba60768463b6473be0dbc498450845
Aaron Parker <aaron@stealthpuppy.com>
2021-12-21 20:58:20 +1100

    Update Install-Apps.ps1

--- Install-Apps.ps1
+++ Install-Apps.ps1
@@ -1,17 +1,18 @@
 <#
-    RUn application install scripts
+    Run application install scripts
 #>
-Set-ExecutionPolicy Bypass -Scope Process -Force
+[CmdletBinding()]
+param()
+
+Set-ExecutionPolicy Bypass -Scope "Process" -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 
 $Scripts = @(
     "https://raw.githubusercontent.com/aaronparker/packer/main/build/rds/01_Rds-PrepImage.ps1",
-    "https://raw.githubusercontent.com/aaronparker/packer/main/build/common/02_Packages.ps1",
     "https://raw.githubusercontent.com/aaronparker/packer/main/build/common/03_RegionLanguage.ps1",
-    "https://raw.githubusercontent.com/aaronparker/packer/main/build/common/04_Customise.ps1",
-    "https://raw.githubusercontent.com/aaronparker/packer/main/build/rds/05_Rds-Roles.ps1",
+    "https://stealthpuppy.com/image-customise/Install.ps1",
     "https://raw.githubusercontent.com/aaronparker/packer/main/build/common/06_SupportFunctions.ps1",
-    "https://raw.githubusercontent.com/aaronparker/packer/main/build/rds/07_MicrosoftVcRedists.ps1",
+    "https://vcredist.com/install.ps1",
     "https://raw.githubusercontent.com/aaronparker/packer/main/build/rds/08_MicrosoftFSLogixApps.ps1",
     "https://raw.githubusercontent.com/aaronparker/packer/main/build/rds/09_MicrosoftEdge.ps1",
     "https://raw.githubusercontent.com/aaronparker/packer/main/build/rds/10_Microsoft365Apps.ps1",
@@ -22,5 +23,5 @@ $Scripts = @(
 )
 
 ForEach ($Script in $Scripts) {
-    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString($Script))
+    Invoke-Expression -Command ((New-Object -TypeName "System.Net.WebClient").DownloadString($Script))
 }
